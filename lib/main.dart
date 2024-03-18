4572e293ef333c4c0e734aacba730859e9bc9b7d
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-18 23:04:00 +0530

    Integrated meals' category screen into main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_bucket/widgets/screens/meals_categories_slw.dart';
+
+ThemeData theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Text(
-          'Hello world!',
-        ),
-      ),
-    ),
+    const App(),
   );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const MealsCategoriesScreen(),
+    );
+  }
 }
