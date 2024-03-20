f89c6129f40cac19b4dc3ef9057c7e395696ec08
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-21 02:47:15 +0530

    Implemented filters screen stateful widget with popScope widget to return filters details to tabs screen to render filters basis meals only.

---
+++ lib/widgets/screens/filters_screen_sfw.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+final Map<String, bool> filters = {
+  'isGlutenFree': false,
+  'isVegan': false,
+  'isVegetarian': false,
+  'isLactoseFree': false,
+};
+
+class FiltersScreenSFW extends StatefulWidget {
+  const FiltersScreenSFW({
+    super.key,
+  });
+
+  @override
+  State<FiltersScreenSFW> createState() => _FiltersScreenSFWState();
+}
+
+class _FiltersScreenSFWState extends State<FiltersScreenSFW> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Apply Filters',
+        ),
+      ),
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (didPop) {
+          if (didPop) return;
+          final bool isFiltered = filters.values.firstWhere(
+            (filter) => filter,
+            orElse: () => false,
+          );
+          if (isFiltered) {
+            Navigator.of(context).pop(filters);
+          } else {
+            Navigator.of(context).pop();
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SwitchListTile(
+                value: filters['isGlutenFree']!,
+                onChanged: (isChecked) {
+                  setState(() {
+                    filters['isGlutenFree'] = isChecked;
+                  });
+                },
+                title: const Text(
+                  'Gluten-free',
+                ),
+                subtitle: const Text(
+                  'Only includes gluten-free meals',
+                ),
+              ),
+              SwitchListTile(
+                value: filters['isLactoseFree']!,
+                onChanged: (isChecked) {
+                  setState(() {
+                    filters['isLactoseFree'] = isChecked;
+                  });
+                },
+                title: const Text(
+                  'Lactose-free',
+                ),
+                subtitle: const Text(
+                  'Only includes lactose-free meals',
+                ),
+              ),
+              SwitchListTile(
+                value: filters['isVegetarian']!,
+                onChanged: (isChecked) {
+                  setState(() {
+                    filters['isVegetarian'] = isChecked;
+                  });
+                },
+                title: const Text(
+                  'Vegetarian',
+                ),
+                subtitle: const Text(
+                  'Only includes vegetarian meals',
+                ),
+              ),
+              SwitchListTile(
+                value: filters['isVegan']!,
+                onChanged: (isChecked) {
+                  setState(() {
+                    filters['isVegan'] = isChecked;
+                  });
+                },
+                title: const Text(
+                  'Vegan',
+                ),
+                subtitle: const Text(
+                  'Only includes vegan meals',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
