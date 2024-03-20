fa368c38dd1b6148025962e978985656b87ddc0d
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-21 02:49:22 +0530

    Implemented setting filters screen for currentScreenIndex 2 and reading filters data returned and applying filters and passing filtered meals to meals categories screen only.

--- lib/widgets/screens/main_tab_bar_screen.dart
+++ lib/widgets/screens/main_tab_bar_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+
+import 'package:meals_bucket/data/meals.dart';
 import 'package:meals_bucket/models/meal_model.dart';
 import 'package:meals_bucket/widgets/screens/category_meals_slw.dart';
+import 'package:meals_bucket/widgets/screens/filters_screen_sfw.dart';
 import 'package:meals_bucket/widgets/screens/meals_categories_slw.dart';
+import 'package:meals_bucket/widgets/widgets/main_drawer_slw.dart';
 
 AppBar appBar = AppBar(
   title: const Text(
@@ -24,7 +28,7 @@ class _MainTabBarScreenState extends State<MainTabBarScreen> {
   List<MealModel> favoriteMealCategories = [];
   AppBar? currentAppBar = appBar;
 
-  void _setCurrentScreen(int index) {
+  void _setCurrentScreen(int index) async {
     if (index == 1) {
       currentAppBar = null;
       setState(() {
@@ -33,6 +37,7 @@ class _MainTabBarScreenState extends State<MainTabBarScreen> {
           favoriteMeals: favoriteMealCategories,
           addRemoveFavorites: _addRemoveFavorites,
           currentTabIndex: currentScreenIndex,
+          filteredMeals: const [],
         );
       });
     }
@@ -44,8 +49,48 @@ class _MainTabBarScreenState extends State<MainTabBarScreen> {
           favoriteMeals: favoriteMealCategories,
           addRemoveFavorites: _addRemoveFavorites,
           currentTabIndex: currentScreenIndex,
+          filteredMeals: const [],
         );
       });
+    }
+
+    if (index == 2) {
+      currentAppBar = null;
+      currentScreenIndex = 2;
+      final mealsFilters = await Navigator.of(context).push<Map<String, bool>>(
+        MaterialPageRoute(builder: (ctx) {
+          return const FiltersScreenSFW();
+        }),
+      );
+      List<MealModel> filteredMeals = [];
+      if (mealsFilters != null) {
+        filteredMeals = meals.where((meal) {
+          return mealsFilters.entries.every((entry) {
+            switch (entry.key) {
+              case 'isGlutenFree':
+                return !entry.value || meal.isGlutenFree;
+              case 'isVegan':
+                return !entry.value || meal.isVegan;
+              case 'isVegetarian':
+                return !entry.value || meal.isVegetarian;
+              case 'isLactoseFree':
+                return !entry.value || meal.isLactoseFree;
+              default:
+                return true;
+            }
+          });
+        }).toList();
+        setState(() {
+          currentAppBar = appBar;
+          currentScreenIndex = 0;
+          currentScreen = MealsCategoriesScreen(
+            favoriteMeals: favoriteMealCategories,
+            addRemoveFavorites: _addRemoveFavorites,
+            currentTabIndex: currentScreenIndex,
+            filteredMeals: filteredMeals,
+          );
+        });
+      }
     }
   }
 
@@ -74,31 +119,37 @@ class _MainTabBarScreenState extends State<MainTabBarScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: currentAppBar,
+      drawer: MainDrawer(
+        onSelectMenuItem: _setCurrentScreen,
+      ),
       body: currentScreen ??
           MealsCategoriesScreen(
             favoriteMeals: favoriteMealCategories,
             addRemoveFavorites: _addRemoveFavorites,
             currentTabIndex: currentScreenIndex,
+            filteredMeals: const [],
           ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentScreenIndex,
-        onTap: _setCurrentScreen,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.set_meal_rounded,
-            ),
-            label: 'Categories',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.star_rate_rounded,
-            ),
-            label: 'Favorites',
-          ),
-        ],
-        type: BottomNavigationBarType.fixed,
-      ),
+      bottomNavigationBar: currentScreenIndex != 2
+          ? BottomNavigationBar(
+              currentIndex: currentScreenIndex,
+              onTap: _setCurrentScreen,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.set_meal_rounded,
+                  ),
+                  label: 'Categories',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.star_rate_rounded,
+                  ),
+                  label: 'Favorites',
+                ),
+              ],
+              type: BottomNavigationBarType.fixed,
+            )
+          : null,
     );
   }
 }
