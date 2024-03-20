5670be81b63c0dc3cd0f5c7efe439bf51ed1362f
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-21 02:45:07 +0530

    Implemented main drawer screen stateless wiget with meals and filters menu options.

---
+++ lib/widgets/widgets/main_drawer_slw.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({
+    super.key,
+    required this.onSelectMenuItem,
+  });
+
+  final void Function(int currentScreenIndex) onSelectMenuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+            ),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.fastfood_rounded,
+                  size: 48,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  'Cooking Up!',
+                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.ramen_dining_rounded,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              onSelectMenuItem(0);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              onSelectMenuItem(2);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
