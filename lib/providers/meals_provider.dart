04d58d78ebce2012c674b084fda10846068aa274
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-23 22:14:37 +0530

    Implemented filters state notifier provider and filtered meals provider in meals_provider.dart.

--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -27,3 +27,45 @@ final favoriteMealsProvider =
     StateNotifierProvider<FavoriteMealsNotifier, List<MealModel>>(
   (ref) => FavoriteMealsNotifier(),
 );
+
+enum Filters { isGlutenFree, isVegan, isVegetarian, isLactoseFree }
+
+class FiltersStateNotifier extends StateNotifier<Map<Filters, bool>> {
+  FiltersStateNotifier()
+      : super({
+          Filters.isGlutenFree: false,
+          Filters.isVegan: false,
+          Filters.isVegetarian: false,
+          Filters.isLactoseFree: false
+        });
+
+  void setFilters(Filters filter, bool isActive) {
+    state = {...state, filter: isActive};
+  }
+}
+
+final filtersStateProvider =
+    StateNotifierProvider<FiltersStateNotifier, Map<Filters, bool>>(
+  (ref) => FiltersStateNotifier(),
+);
+
+final filteredMealsProvider = Provider((ref) {
+  final filtersProvider = ref.watch(filtersStateProvider);
+  final meals = ref.read(mealsProvider);
+  return meals.where((meal) {
+    return filtersProvider.entries.every((entry) {
+      switch (entry.key) {
+        case Filters.isGlutenFree:
+          return !entry.value || meal.isGlutenFree;
+        case Filters.isVegan:
+          return !entry.value || meal.isVegan;
+        case Filters.isVegetarian:
+          return !entry.value || meal.isVegetarian;
+        case Filters.isLactoseFree:
+          return !entry.value || meal.isLactoseFree;
+        default:
+          return true;
+      }
+    });
+  }).toList();
+});
