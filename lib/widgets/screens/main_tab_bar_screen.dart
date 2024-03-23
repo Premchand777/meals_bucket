94851fb03a099566f49b6745c9646c9c54a53744
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-23 22:22:25 +0530

    Converted main tabs screen consumer stateful widget into stateful widget as filtered meals logic is removed from here and made it as a provider and not using that provider here. Removed not needed filtered meals variable anymore and removed sending it to other screens.

--- lib/widgets/screens/main_tab_bar_screen.dart
+++ lib/widgets/screens/main_tab_bar_screen.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:meals_bucket/models/meal_model.dart';
 import 'package:meals_bucket/widgets/screens/category_meals_slw.dart';
 import 'package:meals_bucket/widgets/screens/filters_screen_sfw.dart';
 import 'package:meals_bucket/widgets/screens/meals_categories_slw.dart';
 import 'package:meals_bucket/widgets/widgets/main_drawer_slw.dart';
-
-import 'package:meals_bucket/providers/meals_provider.dart';
 
 AppBar appBar = AppBar(
   title: const Text(
@@ -16,28 +11,27 @@ AppBar appBar = AppBar(
   ),
 );
 
-class MainTabBarScreen extends ConsumerStatefulWidget {
+class MainTabBarScreen extends StatefulWidget {
   const MainTabBarScreen({
     super.key,
   });
 
   @override
-  ConsumerState<MainTabBarScreen> createState() => _MainTabBarScreenState();
+  State<MainTabBarScreen> createState() => _MainTabBarScreenState();
 }
 
-class _MainTabBarScreenState extends ConsumerState<MainTabBarScreen> {
+class _MainTabBarScreenState extends State<MainTabBarScreen> {
   int currentScreenIndex = 0;
   Widget? currentScreen;
   AppBar? currentAppBar = appBar;
-  List<MealModel> filteredMeals = [];
 
   void _setCurrentScreen(int index) async {
     if (index == 1) {
       currentAppBar = null;
       setState(() {
         currentScreenIndex = 1;
-        currentScreen = CategoryMealsScreen(
-          filteredMeals: filteredMeals,
+        currentScreen = const CategoryMealsScreen(
+          filteredMeals: [],
         );
       });
     }
@@ -46,46 +40,18 @@ class _MainTabBarScreenState extends ConsumerState<MainTabBarScreen> {
       currentAppBar = appBar;
       setState(() {
         currentScreenIndex = 0;
-        currentScreen = MealsCategoriesScreen(
-          filteredMeals: filteredMeals,
-        );
+        currentScreen = const MealsCategoriesScreen();
       });
     }
 
     if (index == 2) {
       currentAppBar = null;
       currentScreenIndex = 2;
-      final mealsFilters = await Navigator.of(context).push<Map<String, bool>>(
+      Navigator.of(context).push(
         MaterialPageRoute(builder: (ctx) {
           return const FiltersScreenSFW();
         }),
       );
-      if (mealsFilters != null) {
-        final meals = ref.read(mealsProvider);
-        filteredMeals = meals.where((meal) {
-          return mealsFilters.entries.every((entry) {
-            switch (entry.key) {
-              case 'isGlutenFree':
-                return !entry.value || meal.isGlutenFree;
-              case 'isVegan':
-                return !entry.value || meal.isVegan;
-              case 'isVegetarian':
-                return !entry.value || meal.isVegetarian;
-              case 'isLactoseFree':
-                return !entry.value || meal.isLactoseFree;
-              default:
-                return true;
-            }
-          });
-        }).toList();
-      }
-      setState(() {
-        currentAppBar = appBar;
-        currentScreenIndex = 0;
-        currentScreen = MealsCategoriesScreen(
-          filteredMeals: filteredMeals,
-        );
-      });
     }
   }
 
@@ -97,9 +63,7 @@ class _MainTabBarScreenState extends ConsumerState<MainTabBarScreen> {
         onSelectMenuItem: _setCurrentScreen,
       ),
       body: currentScreen ??
-          const MealsCategoriesScreen(
-            filteredMeals: [],
-          ),
+          const MealsCategoriesScreen(),
       bottomNavigationBar: currentScreenIndex != 2
           ? BottomNavigationBar(
               currentIndex: currentScreenIndex,
